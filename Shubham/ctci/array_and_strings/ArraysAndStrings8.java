663bee6d39c68a6081abe4c87ed08f2dfeced3bd
Shubham Bansal <shbhmbnsl1@gmail.com>
2022-04-29 00:29:59 +0530

    New solution for question 8

--- Shubham/ctci/array_and_strings/ArraysAndStrings8.java
+++ Shubham/ctci/array_and_strings/ArraysAndStrings8.java
@@ -3,67 +3,68 @@ package ctci.array_and_strings;
 import java.util.Arrays;
 
 public class ArraysAndStrings8 {
-
     public static void main(String[] args) {
-        boolean[][] matrix = {{true, false, false}, {false, false, false}, {false, false, true}};
+        int[][] matrix = new int[][]{{1, 2, 0}, {3, 4, 6}, {7, 0, 8}};
         convertToZeroMatrix(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 
-    private static void convertToZeroMatrix(boolean[][] matrix) {
-        boolean rowZero = false;
-        boolean colZero = false;
+    public static void convertToZeroMatrix(int[][] matrix) {
+        boolean firstRow = false;
+        boolean firstCol = false;
 
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i]) {
-                rowZero = true;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
                 break;
             }
         }
 
-        for (int j = 0; j < matrix.length; j++) {
-            if (matrix[j][0]) {
-                colZero = true;
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
                 break;
             }
         }
 
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
-                if (matrix[i][j]) {
-                    matrix[i][0] = true;
-                    matrix[0][j] = true;
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 1; i < matrix[0].length; i++) {
-            if (matrix[0][i]) {
-                for (int j = 0; j < matrix.length; j++) {
-                    matrix[j][i] = true;
-                }
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                makeColZero(matrix, j);
             }
         }
 
         for (int i = 1; i < matrix.length; i++) {
-            if (matrix[i][0]) {
-                for (int j = 0; j < matrix[0].length; j++) {
-                    matrix[i][j] = true;
-                }
+            if (matrix[i][0] == 0) {
+                makeRowZero(matrix, i);
             }
         }
 
-        if (rowZero) {
-            for (int i = 0; i < matrix[0].length; i++) {
-                matrix[0][i] = true;
-            }
+        if (firstRow) {
+            makeRowZero(matrix, 0);
         }
-
-        if (colZero) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][0] = true;
-            }
+        if (firstCol) {
+            makeColZero(matrix, 0);
         }
+    }
 
+    public static void makeRowZero(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    public static void makeColZero(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }
     }
 }
