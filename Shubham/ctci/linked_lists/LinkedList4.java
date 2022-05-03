8d58fb08be146fe0af117d298dabbdc3cc3d6cbe
Shubham Bansal <shbhmbnsl1@gmail.com>
2022-05-04 00:07:25 +0530

    Adding thirteenth question

---
+++ Shubham/ctci/linked_lists/LinkedList4.java
@@ -0,0 +1,26 @@
+package ctci.linked_lists;
+
+import ctci.linked_lists.structure.Node;
+
+public class LinkedList4 {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(6);
+
+        deleteCenterNode(head.next.next);
+        Node.printLinkedList(head);
+
+    }
+
+    public static void deleteCenterNode(Node n) {
+        if (n == null || n.next == null) {
+            return;
+        }
+        Node nex = n.next;
+        n.data = nex.data;
+        n.next = nex.next;
+    }
+}
