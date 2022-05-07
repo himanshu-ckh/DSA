c191bc21bf9cf840b5be7a01c36cc0efc6c396fb
Himanshu <himanshu.c.k.h@gmail.com>
2022-05-07 10:40:07 +0530

    7th May 22 - BST - Closest node

---
+++ Himanshu/Trees/FindClosestInBinarySearchTree.java
@@ -0,0 +1,42 @@
+package Himanshu.ctcci.Trees;
+import java.util.*;
+
+public class FindClosestInBinarySearchTree {
+    static class Program {
+
+        public static int findClosestValueInBst(BST tree, int target) {
+            int close = Integer.MAX_VALUE;
+            int res = -1;
+
+            res = findCloseset(tree, target, close, res);
+            return res;
+        }
+        public static int findCloseset(BST tree, int target, int close, int res){
+            // Write your code here.
+            if(Math.abs(target - tree.value) < close) {
+                close = Math.abs(target - tree.value);
+                res = tree.value;
+            }
+
+            if(tree.value > target && tree.left != null) {
+                res = findCloseset(tree.left, target, close, res);
+            }
+            else if(tree.value < target && tree.right != null) {
+                res = findCloseset(tree.right, target, close, res);
+            }
+
+            return res;
+        }
+
+        static class BST {
+            public int value;
+            public BST left;
+            public BST right;
+
+            public BST(int value) {
+                this.value = value;
+            }
+        }
+    }
+
+}
