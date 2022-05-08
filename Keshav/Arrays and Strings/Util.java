817541cdaa2fa8a84bf87872957f5a6d10028f0c
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2022-05-07 21:54:08 -0400

    rotate Image Leetcode 48

---
+++ Keshav/Arrays and Strings/Util.java
@@ -0,0 +1,13 @@
+package com.dsa.interview.prep;
+
+import java.util.Arrays;
+
+public class Util {
+
+    public void printMatrix(int[][] matrix){
+        for(int[] row : matrix){
+                System.out.println(Arrays.toString(row));
+
+        }
+    }
+}
