7ea63dfd0b63edb183b5f86730bf75e61b44185a
Himanshu <himanshu.c.k.h@gmail.com>
2022-05-18 09:13:51 +0530

    18th May 22 - Invert Binary tree

---
+++ Himanshu/Trees/InvertBinaryTree.java
@@ -0,0 +1,31 @@
+package Himanshu.ctcci.Trees;
+import java.util.*;
+
+//https://www.algoexpert.io/questions/Invert%20Binary%20Tree
+public class InvertBinaryTree {
+    static class Program {
+        public static void invertBinaryTree(BinaryTree tree) {
+            // Write your code here.
+            if(tree == null) {
+                return;
+            }
+
+            BinaryTree temp = tree.left;
+            tree.left = tree.right;
+            tree.right = temp;
+            invertBinaryTree(tree.left);
+            invertBinaryTree(tree.right);
+        }
+
+        static class BinaryTree {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value) {
+                this.value = value;
+            }
+        }
+    }
+
+}
