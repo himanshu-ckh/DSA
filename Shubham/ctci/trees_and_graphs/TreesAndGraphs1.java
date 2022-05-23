e89e1b84dedefaa133a2c643ba8ccd6d7c946edc
Shubham Bansal <shbhmbnsl1@gmail.com>
2022-05-24 00:38:57 +0530

    Adding 24th question

---
+++ Shubham/ctci/trees_and_graphs/TreesAndGraphs1.java
@@ -0,0 +1,31 @@
+package ctci.trees_and_graphs;
+
+
+import ctci.trees_and_graphs.structure.Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreesAndGraphs1 {
+    public static void bfs(int source, Graph graph) {
+        boolean[] visited = new boolean[graph.vertices];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
+        System.out.println(source);
+
+        while (!queue.isEmpty()) {
+            int poll = queue.poll();
+
+            LinkedList<Integer> neighbours = graph.adj[poll];
+            for (Integer neighbour: neighbours) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    queue.add(neighbour);
+                    graph.parents[neighbour] = poll;
+                    System.out.println(neighbour);
+                }
+            }
+        }
+    }
+}
