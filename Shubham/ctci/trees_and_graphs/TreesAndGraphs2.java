a8947c908366ccd4dbb63564adf7c4cbc9553bd3
Shubham Bansal <shbhmbnsl1@gmail.com>
2022-05-26 00:37:06 +0530

    Adding 25th question

---
+++ Shubham/ctci/trees_and_graphs/TreesAndGraphs2.java
@@ -0,0 +1,24 @@
+package ctci.trees_and_graphs;
+
+import ctci.trees_and_graphs.structure.Graph;
+
+import java.util.LinkedList;
+
+public class TreesAndGraphs2 {
+    public static void dfs(int source, Graph graph) {
+        boolean[] visited = new boolean[graph.vertices];
+
+        dfsHelper(source, visited, graph);
+    }
+
+    private static void dfsHelper(int source, boolean[] visited, Graph graph) {
+        visited[source] = true;
+        System.out.println(source);
+        LinkedList<Integer> neighbours = graph.adj[source];
+        for (Integer neihbour: neighbours) {
+            if (!visited[neihbour]) {
+                dfsHelper(neihbour, visited, graph);
+            }
+        }
+    }
+}
