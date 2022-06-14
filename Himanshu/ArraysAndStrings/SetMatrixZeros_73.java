87ac023a8f4e38156040076cfd610c43e208c41b
Himanshu <himanshu.c.k.h@gmail.com>
2022-06-14 17:11:02 +0530

    14th June 22: Set matrix zeros

---
+++ Himanshu/ArraysAndStrings/SetMatrixZeros_73.java
@@ -0,0 +1,32 @@
+package Himanshu.ctcci.ArraysAndString;
+
+import java.util.HashSet;
+
+public class SetMatrixZeros_73 {
+    static class Solution {
+        public void setZeroes(int[][] matrix) {
+            int length = matrix.length;
+            int cols = matrix[0].length;
+
+            HashSet<Integer> setRow = new HashSet<>();
+            HashSet<Integer> setCol = new HashSet<>();
+
+            for(int i=0; i<length; i++) {
+                for(int j=0; j<cols; j++) {
+                    if(matrix[i][j] == 0) {
+                        setRow.add(i);
+                        setCol.add(j);
+                    }
+                }
+            }
+
+            for(int r=0; r<length; r++) {
+                for(int c=0; c<cols; c++) {
+                    if(setRow.contains(r) || setCol.contains(c)) {
+                        matrix[r][c] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
